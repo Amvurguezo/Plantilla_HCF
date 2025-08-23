236817e4137bdb18ceb68f3018d3319bcb357481
Amvurguezo <155598975+Amvurguezo@users.noreply.github.com>
2025-08-22 21:23:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -92,11 +92,11 @@ document.getElementById('sistemIntegracion').addEventListener('click', () => {
 });
 
 document.getElementById('cambioPassCorreo').addEventListener('click', () => {
-    copyToClipboardText('Buen día\n\nSe le informa que se realiza el cambio de contraseña para el correo de (Nombre usuario)\n\nCorreo:\nContraseña:\n\nPor favor, recuerde cambiar la contraseña en el próximo inicio de sesión para garantizar la seguridad de su cuenta\n\n');
+    copyToClipboardText('Buen día\n\nSe le informa que se realiza el cambio de contraseña para el correo de (Nombre usuario)\n\nCorreo: \nContraseña: \n\nPor favor, recuerde cambiar la contraseña en el próximo inicio de sesión para garantizar la seguridad de su cuenta\n\n');
 });
 
-document.getElementById('accesoCarpeta').addEventListener('click', () => {
-    copyToClipboardText('Estimado,\n\nJunto con saludar, con el fin de continuar con su solicitud por favor indicar la siguiente información:\n\n- Ruta: \\servidor\carpetas\subcarpeta\n- Tipo de permisos (Lectura o lectura y escritura) \n\nGracias.\n\n');
+document.getElementById('extensionCorreo').addEventListener('click', () => {
+    copyToClipboardText('Buen día\n\nSe le informa que se realiza la extensión de cuenta para el correo de (Nombre usuario)\n\nCorreo: \nContraseña: \n\nPor favor, recuerde cambiar la contraseña en el próximo inicio de sesión para garantizar la seguridad de su cuenta.\n\n');
 });
 
 document.getElementById('powerBi').addEventListener('click', () => {
@@ -114,6 +114,7 @@ document.getElementById('googleDrive').addEventListener('click', () => {
 document.getElementById('caServiceDesk').addEventListener('click', () => {
     copyToClipboardText('Buenas tardes\n\nPara poder continuar con su requerimiento es necesario la siguiente información del usuario a crear:\n\n\n\nNombre completo:\nRut:\nCorreo electrónico:\nTipo de acceso: (Usuario Employee -Permite ver los tickets de la persona) (Usuario Analyst - con acceso a gestión de tickets)\n\n');
 });
+
 
 
 
